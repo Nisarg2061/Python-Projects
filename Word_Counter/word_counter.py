4311021436eec6a9e385fa02b1af32b201901a1d
Nisarg2061 <khodkenisarg@gmail.com>
2023-12-31 16:34:57 +0530

    Added spaces

--- Word_Counter/word_counter.py
+++ Word_Counter/word_counter.py
@@ -1,30 +1,25 @@
 print("")
 input_file=input("Enetr a file to count its words:")
+
 try:
     data = open(input_file)
 except:
     print("No such file found!!")
     quit()
+
 line = data.read()
+
 words = line.split()
+
 count = dict()
+
 for i in words:
     count[i]=count.get(i,0)+1
+
 sum=0
+
 for i in count:
     sum=sum+count[i]
+
 print("")
 print("Total Number of words:",sum)
-
-
-
-
-
-
-
-
-
-
-
-
-
